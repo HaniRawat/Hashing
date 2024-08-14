3e9007792605bfe3de1641c57c79f347a5244910
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2024-08-14 23:05:40 +0530

    solved

---
+++ Joey_Doesnt_Share_Food.cpp
@@ -0,0 +1,123 @@
+//{ Driver Code Starts
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+
+#define MAX 1000010
+
+vector<int> g[MAX];
+int pa[MAX], pb[MAX];
+long long iter, was[MAX];
+
+class Solution {
+private:
+    vector<vector<int>> input;
+
+public:
+    void initialize(int n, vector<vector<int>> &nums)
+    {
+        input = nums;
+
+        for (int i = 0; i < MAX; i++)   g[i].clear();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 6; j++) {
+                g[nums[i][j]].push_back(i);
+            }
+        }
+        
+        for (int i = 0; i < MAX; i++) {
+            random_shuffle(g[i].begin(), g[i].end());
+        }
+    }
+
+    bool dfs(int v) {
+        was[v] = iter;
+        
+        for (int j : g[v]) {
+            if (pb[j] == -1) {
+                pa[v] = j;
+                pb[j] = v;
+                
+                return true;
+            }
+        }
+        
+        for (int j : g[v]) {
+            if (was[pb[j]] != iter) {
+                if (dfs(pb[j])) {
+                    pa[v] = j;
+                    pb[j] = v;
+                    
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+	int MaxmimumLength(vector<vector<int>> nums)
+	{
+	    int n = nums.size();
+	    
+        initialize(n, nums);
+
+        for (int i = 0; i < MAX; i++) {
+            pa[i] = -1;
+            was[i] = -1;
+        }
+        for (int j = 0; j < n; j++) {
+            pb[j] = -1;
+        }
+
+        int ans = 0, rr = 0;
+        iter = 0;
+        
+        for (int ll = 1; ll < MAX; ll++) {
+            rr = max(rr, ll - 1);
+            
+            while (true) {
+                iter++;
+                
+                if (dfs(rr + 1))    rr++;
+                else    break;
+            }
+            
+            ans = max(ans, rr - ll + 1);
+            
+            if (pa[ll] != -1) {
+                pb[pa[ll]] = -1;
+                pa[ll] = -1;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>>nums(n, vector<int>(6, 0));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < 6; j++)
+				cin >> nums[i][j];
+		}
+		Solution obj;
+		int ans = obj.MaxmimumLength(nums);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+
+// } Driver Code Ends
